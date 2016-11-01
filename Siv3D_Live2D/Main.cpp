b559b2cc18d3e05a5b5516587c0a40ed71d6dbd7
oigami <oigami013@yahoo.co.jp>
2016-11-02 00:19:17 +0900

    ライブラリの追加

--- Siv3D_Live2D/Main.cpp
+++ Siv3D_Live2D/Main.cpp
@@ -1,14 +1,21 @@
 ﻿
 # include <Siv3D.hpp>
-
+#include "lib/siv3d_live2d.h"
 void Main()
 {
-	const Font font(30);
+  const Font font(30);
 
-	while (System::Update())
-	{
-		font(L"ようこそ、Siv3D の世界へ！").draw();
+  Live2D::Initialize();
 
-		Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
-	}
+  auto model = Live2D::LoadModel(L"res\\epsilon\\Epsilon2.1.moc", { L"res\\epsilon\\Epsilon2.1.2048\\texture_00.png" });
+
+  while ( System::Update() )
+  {
+    font(L"ようこそ、Siv3D の世界へ！").draw();
+
+    Live2D::SampleUpdate(model);
+    Live2D::AddDrawObject(model);
+
+    Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
+  }
 }
